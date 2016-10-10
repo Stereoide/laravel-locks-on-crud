e453dc8ba958719a90c0b68996d670f047c4b7e3
Tobias Wanders <Stereoide@gmail.com>
2016-10-10 15:13:42 +0200

    Initial commit to get things started

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # laravel-locks-on-crud
-Provides an automatic locking mechanism on model CRUD-events
+
+[![Latest Version on Packagist][ico-version]][link-packagist]
+[![Software License][ico-license]](LICENSE.md)
+[![Build Status][ico-travis]][link-travis]
+[![Coverage Status][ico-scrutinizer]][link-scrutinizer]
+[![Quality Score][ico-code-quality]][link-code-quality]
+[![Total Downloads][ico-downloads]][link-downloads]
+
+This is where your description should go. Try and limit it to a paragraph or two, and maybe throw in a mention of what
+PSRs you support to avoid any confusion with users and contributors.
+
+## Install
+
+Via Composer
+
+``` bash
+$ composer require stereoide/laravel-locks-on-crud
+```
+
+## Usage
+
+``` php
+$skeleton = new stereoide\laravel-locks-on-crud();
+echo $skeleton->echoPhrase('Hello, League!');
+```
+
+## Change log
+
+Please see [CHANGELOG](CHANGELOG.md) for more information what has changed recently.
+
+## Testing
+
+``` bash
+$ composer test
+```
+
+## Contributing
+
+Please see [CONTRIBUTING](CONTRIBUTING.md) and [CONDUCT](CONDUCT.md) for details.
+
+## Security
+
+If you discover any security related issues, please email Stereoide@gmail.com instead of using the issue tracker.
+
+## Credits
+
+- [Tobias Wanders][link-author]
+- [All Contributors][link-contributors]
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE.md) for more information.
+
+[ico-version]: https://img.shields.io/packagist/v/stereoide/laravel-locks-on-crud.svg?style=flat-square
+[ico-license]: https://img.shields.io/badge/license-MIT-brightgreen.svg?style=flat-square
+[ico-travis]: https://img.shields.io/travis/stereoide/laravel-locks-on-crud/master.svg?style=flat-square
+[ico-scrutinizer]: https://img.shields.io/scrutinizer/coverage/g/stereoide/laravel-locks-on-crud.svg?style=flat-square
+[ico-code-quality]: https://img.shields.io/scrutinizer/g/stereoide/laravel-locks-on-crud.svg?style=flat-square
+[ico-downloads]: https://img.shields.io/packagist/dt/stereoide/laravel-locks-on-crud.svg?style=flat-square
+
+[link-packagist]: https://packagist.org/packages/stereoide/laravel-locks-on-crud
+[link-travis]: https://travis-ci.org/stereoide/laravel-locks-on-crud
+[link-scrutinizer]: https://scrutinizer-ci.com/g/stereoide/laravel-locks-on-crud/code-structure
+[link-code-quality]: https://scrutinizer-ci.com/g/stereoide/laravel-locks-on-crud
+[link-downloads]: https://packagist.org/packages/stereoide/laravel-locks-on-crud
+[link-author]: https://github.com/stereoide
+[link-contributors]: ../../contributors
---
+++ src/LocksOnCrud.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Stereoide\LocksOnCrud;
+
+trait LocksOnCrud
+{
+    /**
+     * The internal lock id this trait method use.
+     *
+     * @var int
+     */
+    protected $automaticLockIds = null;
+
+    public static function bootLocksOnCrud()
+    {
+        /* Set event listeners so we can jump in every time a model is being created, updated or deleted */
+
+        static::creating(function ($model) {
+            $locking = app('locking');
+            
+            /* Since this model does not yet exist we need to apply a read lock on its parent if necessary */
+            
+            $attributes = $model->getAttributes();
+            if (isset($attributes['parent_id'])) {
+                $locking->acquireReadLock($model->parent_id);
+            }
+            
+            
+        });
+
+        static::created(function ($model) {
+            $locking = app('locking');
+            
+            /* Release the read lock we created on the parent */
+
+            $attributes = $model->getAttributes();
+            if (isset($attributes['parent_id'])) {
+                $locking->releaseLocksByEntityId($model->parent_id, $locking->READ_LOCK);
+            }
+        });
+
+        static::updating(function ($model) {
+            $locking = app('locking');
+            
+            /* Acquire write lock */
+
+            $model->automaticLockIds = $locking->acquireWriteLock($model->id);
+            sleep(10);
+        });
+
+        static::updated(function ($model) {
+            $locking = app('locking');
+            
+            /* Release the automatically acquired locks */
+
+            if (!empty($model->automaticLockIds)) {
+                $locking->releaseLocks($model->automaticLockIds);
+            }
+        });
+
+        static::deleting(function ($model) {
+            $locking = app('locking');
+            
+            /* Read lock this models parents if necessary */
+
+            $attributes = $model->getAttributes();
+            if (isset($attributes['parent_id'])) {
+                $model->automaticLockIds[] = $locking->readLockParentSequence($model->id);
+            }
+
+            /* Write lock this model */
+
+            $model->automaticWriteLockId[] = $locking->acquireWriteLock($model->id);
+
+            /* Refresh our locks just in case acquiring the write lock took longer than expected */
+
+            $locking->refreshLocks();
+        });
+
+        static::deleted(function ($model) {
+            $locking = app('locking');
+            
+            /* Release the automatically acquired locks */
+
+            if (!empty($model->automaticLockIds)) {
+                $locking->releaseLocks($model->automaticLockIds);
+            }
+        });
+    }
+}
